8bf56ca7fd84e586ab77cb095b3c1462b5f4d410
FloodDis <vlad.sechenov@mail.ru>
2022-02-05 19:22:50 +0700

    Изменил поле  _news класса NewsProgram с типа vector на тип queue

--- Новости (Задание 3)/NewsProgram.h
+++ Новости (Задание 3)/NewsProgram.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <vector>
+#include <queue>
 #include <string>
 using namespace std;
+
 class NewsProgram
 {
 private:
-	vector <string> _news;
+	queue <string> _news;
 public:
 	NewsProgram(string news);
 	void SetNewsProgram(string news);
