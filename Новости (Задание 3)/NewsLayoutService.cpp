79cc4c05c4503b6f9c00803e4c5d55539f9bfa52
FloodDis <vlad.sechenov@mail.ru>
2022-02-05 21:59:05 +0700

    Написал реализацию метода SetNewsProgram класса NewsLayoutService

--- Новости (Задание 3)/NewsLayoutService.cpp
+++ Новости (Задание 3)/NewsLayoutService.cpp
@@ -2,10 +2,27 @@
 
 void NewsLayoutService::SetNewsProgram(int newsCount)
 {
-	string buffer;
+	string* buffer=new string[newsCount];
+	int* bufferSize = new int[newsCount];
 	for (int i = 0; i < newsCount; i++)
 	{
-
+		buffer[i] = _newsList.GetNewsProgram();
+	}
+	for (int i = 0; i < newsCount; i++)
+	{
+		int j = 0;
+		while (buffer[i][j] != '\0')
+		{
+			j++;
+		}
+		bufferSize[i] = j;
+	}
+	for (int i = 1; i < newsCount; i++)
+	{
+		for (int j = i; j > 0 && bufferSize[j - 1] > bufferSize[j]; j--)
+		{
+			swap(bufferSize[j - 1], bufferSize[j]);
+		}
 	}
 }
 
